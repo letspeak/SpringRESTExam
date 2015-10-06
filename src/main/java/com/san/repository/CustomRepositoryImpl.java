1defd3477a6f02ae07f24ab9fd03e0020910fe54
lespeak <letspeak@users.noreply.github.com>
2015-10-05 22:57:13 -0500

    one step ahead

--- src/main/java/com/san/repository/CustomRepositoryImpl.java
+++ src/main/java/com/san/repository/CustomRepositoryImpl.java
@@ -9,18 +9,25 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
+import com.san.model.EmailFromRecipient;
 import com.san.model.EmailHeader;
 
 @RequiredArgsConstructor
 public class CustomRepositoryImpl implements CustomRepository {
 
 	private MongoOperations operations;
+	@Autowired
+	MongoTemplate template;
 	@Override
 	public List<EmailHeader> findAll(Sort sort) {
 
@@ -32,9 +39,9 @@ public class CustomRepositoryImpl implements CustomRepository {
 				), EmailHeader.class);
 		return results.getMappedResults();
 	}
-	public List<EmailHeader> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<EmailFromRecipient> findAll() {
+		Query q = new Query(new Criteria("messages"));
+		return template.find(q, EmailFromRecipient.class);
 	}
 	public <S extends EmailHeader> S insert(S arg0) {
 		// TODO Auto-generated method stub
