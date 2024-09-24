b20d495d8a0df7a601788e4578edd21174b67129
letspeak <letspeak@users.noreply.github.com>
2024-09-23 20:15:05 -0500

    Update XSSController.java

--- src/main/java/com/san/controller/XSSController.java
+++ src/main/java/com/san/controller/XSSController.java
@@ -6,11 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@Controller
+@RestController
 public class XSSController {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
